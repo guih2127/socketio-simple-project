bb3a51cfc5ce6f4159be29d1d838596df1ec274c
Guilherme Reis Araújo <guih2127@gmail.com>
2022-06-03 12:08:50 -0300

    Socket and HTML configurations to make the CHAT happens

--- server.js
+++ server.js
@@ -63,6 +63,34 @@ io.attach(app, {
   allowEIO3: true,
 });
 
+var users = {};
+
 io.on("connection", (socket) => {
   console.log("👾 New socket connected! >>", socket.id);
+
+  // handles new connection
+  socket.on("new-connection", (data) => {
+    // captures event when new clients join
+    console.log(`new-connection event received`, data);
+    // adds user to list
+    users[socket.id] = data.username;
+    console.log("users :>> ", users);
+    // emit welcome message event
+    socket.emit("welcome-message", {
+      user: "server",
+      message: `Welcome to this Socket.io chat ${data.username}. There are ${
+        Object.keys(users).length
+      } users connected`,
+    });
+  });
+
+  // handles message posted by client
+  socket.on("new-message", (data) => {
+    console.log(`👾 new-message from ${data.user}`);
+    // broadcast message to all sockets except the one that triggered the event
+    socket.broadcast.emit("broadcast-message", {
+      user: users[data.user],
+      message: data.message,
+    });
+  });
 });
